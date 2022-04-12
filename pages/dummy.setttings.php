904639633733161d80d05ebd4348fa7cfc0950ab
Alexander Walther <aw@alexplus.de>
2022-04-12 08:34:31 +0200

    example settings page

--- pages/dummy.setttings.php
+++ pages/dummy.setttings.php
@@ -1,1 +1,15 @@
 <?php
+#
+$addon = rex_addon::get('dummy');
+
+$form = rex_config_form::factory($addon->name);
+
+$field = $form->addInputField('text', 'mytextfield', null, ["class" => "form-control"]);
+$field->setLabel(rex_i18n::msg('dummy_config_mytextfield_label'));
+$field->setNotice(rex_i18n::msg('dummy_config_mytextfield_notice'));
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'edit', false);
+$fragment->setVar('title', $addon->i18n('dummy_config'), false);
+$fragment->setVar('body', $form->get(), false);
+echo $fragment->parse('core/page/section.php');
