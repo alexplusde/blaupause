541fa730ea4a2b91d27b2a9e377c01eab6f44b0e
alxndr-w <alxndr-w@users.noreply.github.com>
2024-02-11 21:59:43 +0000

    Apply php-cs-fixer changes

--- lib/blaupause.php
+++ lib/blaupause.php
@@ -1,6 +1,6 @@
 <?php
 
-class blaupause extends \rex_yform_manager_dataset
+class blaupause extends rex_yform_manager_dataset
 {
     // https://github.com/yakamara/redaxo_yform/blob/master/docs/04_yorm.md#yorm-mit-eigener-model-class-verwenden
     // Lasse dir die Klasse anhand deines Tablesets selbst bauen: https://github.com/alexplusde/ymca
