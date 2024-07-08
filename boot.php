4d71877644d64b813621c228563663294966ea08
Alexander Walther <mail@alexplus.de>
2024-07-08 11:38:57 +0200

    `+`-Schaltfläche im Hauptmenü zum Hinzufügen neuer Einträge

--- boot.php
+++ boot.php
@@ -1,5 +1,19 @@
 <?php
 
+namespace Bauthor\Blaupause;
+
+use rex_addon;
+use rex_config;
+use rex_yform_manager_dataset;
+use rex;
+use rex_extension;
+use rex_extension_point;
+use rex_cronjob_manager;
+use rex_view;
+use rex_csrf_token;
+use rex_url;
+use rex_yform_manager_table;
+
 // Die boot.php wird bei jedem Seitenaufruf im Frontend und Backend aufgeführt, je nach Reihenfolge von Abhängigkeiten in der package.yml vor oder nach anderen Addons.
 
 // Beispiel YOrm Model-Klasse registrieren, wenn das Addon mit einer eigenen YForm Tabelle kommt.
@@ -7,7 +21,7 @@
 if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
     rex_yform_manager_dataset::setModelClass(
         'rex_blaupause',
-        blaupause::class
+        Blaupause::class
     );
 }
 */
@@ -33,3 +47,23 @@ if (rex::isBackend() && rex::getUser()) {
     rex_view::addJsFile($this->getAssetsUrl('backend.js'));
 }
 */
+
+// YForm-Tabelle? `+`-Button im Hauptmenü hinzufügen
+/* 
+if(rex::isBackend()) {
+    $addon = rex_addon::get('blaupause');
+    $pages = $addon->getProperty('pages');
+    
+    $_csrf_key = rex_yform_manager_table::get('rex_blaupause')->getCSRFKey();
+    $token = rex_csrf_token::factory($_csrf_key)->getUrlParams();
+
+    $params = array();
+    $params['table_name'] = 'rex_blaupause';
+    $params['rex_yform_manager_popup'] = '0';
+    $params['_csrf_token'] = $token['_csrf_token'];
+    $params['func'] = 'add';
+
+    $pages['blaupause']['title'] .= ' <a style="position: absolute; top: 0; right: 0; padding: 5px; margin: 8px 19px 8px 8px" href="'.rex_url::backendPage('blaupause/table', $params) .'" class="label label-default pull-right">+</a>';
+    $addon->setProperty('pages', $pages);
+}
+    */
